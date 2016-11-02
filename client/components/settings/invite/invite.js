5697dd931c46baccb8af7d7d91f2bee723e3c56a
Akram Saouri <akram@hiddenfounders.com>
2016-11-02 16:14:22 +0000

    some progress on invite page

--- client/components/settings/invite/invite.js
+++ client/components/settings/invite/invite.js
@@ -2,21 +2,44 @@ import {
     Session
 } from 'meteor/session';
 
+import {
+    validateFields
+} from '/lib/helpers';
+
+Template.invite.rendered = () => {
+    Session.setDefault('emails', []);
+};
 
 Template.invite.events({
     'click #finish' (e) {
         const email = $('#email').val();
         const message = $('#message').val();
-        if (email.length === 0)
-            return sAlert.error('you need to enter at least one participant email');
+        // if (Session.get('emails').length === 0){
+        //   sAlert.error('you need to enter at least one participant email');
+        //   return e.preventDefault();
+        // }
+        console.log(Session.get('emails'));
         Session.set('invite', {
             email,
             message
         });
-        console.log(Session.get('general'));
-        console.log(Session.get('time'));
-        console.log(Session.get('invite'));
+        // console.log(Session.get('general'));
+        // console.log(Session.get('time'));
+        // console.log(Session.get('invite'));
         alert('done');
+        return e.preventDefault();
+    },
+    'keydown #email' (e) {
+        const email = $('#email').val();
+        if (e.which === 13) {
+            if (!validateFields(email))
+                return e.preventDefault();
+            const emails = Session.get('emails');
+            emails.push(email);
+            Session.set('emails', [...new Set([...emails])]);
+            $('#email').val('');
+        }
     }
+    // TODO: HERE
     // TODO: Dynamically add new emails
 });
